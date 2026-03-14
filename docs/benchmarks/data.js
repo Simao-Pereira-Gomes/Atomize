14eb3cbea7795fd2ecb119aa288e55db7b2423a9
github-action-benchmark <github@users.noreply.github.com>
2026-03-14 11:18:23 +0000

    add Atomize Benchmarks (customSmallerIsBetter) benchmark result for be8c7e5eea2602198b039241cd601b9d3c642d3b

---
+++ docs/benchmarks/data.js
@@ -0,0 +1,65 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1773487103164,
+  "repoUrl": "https://github.com/Simao-Pereira-Gomes/Atomize",
+  "entries": {
+    "Atomize Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "name": "Simao-Pereira-Gomes",
+            "username": "Simao-Pereira-Gomes",
+            "email": "simaogomes75@gmail.com"
+          },
+          "committer": {
+            "name": "Simao-Pereira-Gomes",
+            "username": "Simao-Pereira-Gomes",
+            "email": "simaogomes75@gmail.com"
+          },
+          "id": "be8c7e5eea2602198b039241cd601b9d3c642d3b",
+          "message": "chore: refactor gh-pages branch initialization logic",
+          "timestamp": "2026-03-14T11:17:33Z",
+          "url": "https://github.com/Simao-Pereira-Gomes/Atomize/commit/be8c7e5eea2602198b039241cd601b9d3c642d3b"
+        },
+        "date": 1773487102578,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Validate 50 tasks",
+            "value": 70181.72334931759,
+            "unit": "ns"
+          },
+          {
+            "name": "Validate 100 tasks",
+            "value": 100233.15370823146,
+            "unit": "ns"
+          },
+          {
+            "name": "Validate 200 tasks",
+            "value": 189755.85550660794,
+            "unit": "ns"
+          },
+          {
+            "name": "Validate 500 tasks",
+            "value": 453133.7717536814,
+            "unit": "ns"
+          },
+          {
+            "name": "Resolve 100 tasks (Chain)",
+            "value": 232793.79640084686,
+            "unit": "ns"
+          },
+          {
+            "name": "Resolve 500 tasks (Chain)",
+            "value": 4145901.5361445784,
+            "unit": "ns"
+          },
+          {
+            "name": "Build Map 500 tasks",
+            "value": 56173.81273298019,
+            "unit": "ns"
+          }
+        ]
+      }
+    ]
+  }
+}
