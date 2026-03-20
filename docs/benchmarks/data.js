4aaa0b0dc67f7c8b905301f0d7c7e161423e281b
github-action-benchmark <github@users.noreply.github.com>
2026-03-20 16:45:39 +0000

    add Atomize Benchmarks (customSmallerIsBetter) benchmark result for b5262853ad5731a105e28a5b3ff38f83cd07544c

--- docs/benchmarks/data.js
+++ docs/benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1773487103164,
+  "lastUpdate": 1774025139431,
   "repoUrl": "https://github.com/Simao-Pereira-Gomes/Atomize",
   "entries": {
     "Atomize Benchmarks": [
@@ -56,6 +56,65 @@ window.BENCHMARK_DATA = {
           {
             "name": "Build Map 500 tasks",
             "value": 56173.81273298019,
+            "unit": "ns"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "simaogomes75@gmail.com",
+            "name": "Simao-Pereira-Gomes",
+            "username": "Simao-Pereira-Gomes"
+          },
+          "committer": {
+            "email": "simaogomes75@gmail.com",
+            "name": "Simao-Pereira-Gomes",
+            "username": "Simao-Pereira-Gomes"
+          },
+          "distinct": true,
+          "id": "b5262853ad5731a105e28a5b3ff38f83cd07544c",
+          "message": "chore : update package version and add build/validation scripts",
+          "timestamp": "2026-03-20T16:44:14Z",
+          "tree_id": "dd90c39e62ff6e7943b2c3753e914af9d50737d2",
+          "url": "https://github.com/Simao-Pereira-Gomes/Atomize/commit/b5262853ad5731a105e28a5b3ff38f83cd07544c"
+        },
+        "date": 1774025139186,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Validate 50 tasks",
+            "value": 47880.42600385762,
+            "unit": "ns"
+          },
+          {
+            "name": "Validate 100 tasks",
+            "value": 77557.56750780437,
+            "unit": "ns"
+          },
+          {
+            "name": "Validate 200 tasks",
+            "value": 141270.7217769636,
+            "unit": "ns"
+          },
+          {
+            "name": "Validate 500 tasks",
+            "value": 368008.97539639147,
+            "unit": "ns"
+          },
+          {
+            "name": "Resolve 100 tasks (Chain)",
+            "value": 215333.77937806875,
+            "unit": "ns"
+          },
+          {
+            "name": "Resolve 500 tasks (Chain)",
+            "value": 3958743.408045977,
+            "unit": "ns"
+          },
+          {
+            "name": "Build Map 500 tasks",
+            "value": 53039.6797282204,
             "unit": "ns"
           }
         ]
